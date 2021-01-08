994877bfd3e8a42dab43f71fe8df1aae82694829
Kyle Pykonen <kylepyk@gmail.com>
2021-01-07 20:29:38 -0800

    Fixed u server.js to connect with mongo more easily

--- server.js
+++ server.js
@@ -16,9 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/googlebooks"
-);
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
 
 mongoose.connect(MONGODB_URI, {
   useUnifiedTopology: true,
